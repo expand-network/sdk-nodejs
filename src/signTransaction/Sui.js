85bf140353423937fe58b73ca9fbe0974062d927
Sumit Singh Bisht <111837209+SumitCMLab@users.noreply.github.com>
2023-06-12 14:05:08 +0530

    SDK signTransaction for SUI blockchain

--- src/signTransaction/Sui.js
+++ src/signTransaction/Sui.js
@@ -1,4 +1,5 @@
-const { TransactionBlock , ED25519Keypair, RawSigner } = require('@mysten/sui.js')
+const { TransactionBlock , Ed25519Keypair, RawSigner } = require('@mysten/sui.js');
+const { fromB64 } = require("@mysten/bcs");
 
 module.exports = {
 
@@ -7,16 +8,19 @@ module.exports = {
         try{
             const secretKey = options.privateKey;
             const privateKeyBase64 = Buffer.from(secretKey,"hex").toString("base64"); // Convert hex to base64 string
-            const keypair = ED25519Keypair.fromSecretKey(fromB64(privateKeyBase64));
+            const keypair = Ed25519Keypair.fromSecretKey(fromB64(privateKeyBase64));
             const signer = new RawSigner(keypair, web3);
             const tx = new TransactionBlock();
+            console.log(tx);
             const [coin] = tx.splitCoins(tx.gas, [tx.pure(transactionObject.value)]);
+            console.log([coin]);
             tx.transferObjects(
                 [coin],
                 tx.pure(
                     transactionObject.to
                 )
             );
+            console.log(tx);
             const signedTransaction = await signer.signTransactionBlock({
                 transactionBlock: tx,
             });
@@ -27,4 +31,4 @@ module.exports = {
 
         }
     }
-}
+};
