efe07374e9d96091a380d08df622abcb416268a2
Sumit Singh Bisht <111837209+SumitCMLab@users.noreply.github.com>
2023-06-15 12:59:05 +0530

    comments added for better readability

--- src/signTransaction/Sui.js
+++ src/signTransaction/Sui.js
@@ -6,21 +6,29 @@ module.exports = {
     signTransactionSui: async (web3,transactionObject, options)=> {
 
         try{
+            // get the secretkey from options
             const secretKey = options.privateKey;
             const privateKeyBase64 = Buffer.from(secretKey,"hex").toString("base64"); // Convert hex to base64 string
+            // Create the keypair from converted privaye key
             const keypair = Ed25519Keypair.fromSecretKey(fromB64(privateKeyBase64));
+            // Create a signer with the provided keypair and network
             const signer = new RawSigner(keypair, web3);
+            // Create the transaction with given input
             const tx = new TransactionBlock();
+            // Currently we support sui coin transfer
             const [coin] = tx.splitCoins(tx.gas, [tx.pure(transactionObject.value)]);
+            // Add the instruction
             tx.transferObjects(
                 [coin],
                 tx.pure(
                     transactionObject.to
                 )
             );
+            // Sign the transaction Block
             const signedTransaction = await signer.signTransactionBlock({
                 transactionBlock: tx,
             });
+            // Return the raw Transaction
             return {"rawTransaction":signedTransaction};
 
         } catch(error){
