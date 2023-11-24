245b27f4cf28a39faa4b901093cf307a6f820b9d
SumitCMLab <sumit@ix.cumberlandlabs.io>
2023-11-24 14:50:44 +0530

    TON Wallet implemented

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,8 +1,12 @@
 const { Wallet } = require('../adapters/Wallet');
 const { WalletFordefi } = require('../adapters/WalletFordefi');
-const { WalletDFNS } = require('../adapters/WalletDFNS')
+const { WalletDFNS } = require('../adapters/WalletDFNS');
+const { WalletTON } = require('../adapters/WalletTON');
+
+
 module.exports = {
     Wallet,
     WalletFordefi,
-    WalletDFNS
+    WalletDFNS,
+    WalletTON
 };
