10e664745391a719ee0ff5f34b21a7fe3a34edbe
SumitCMLab <sb152377@gmail.com>
2023-12-15 16:58:49 +0530

    examples added

---
+++ test/circleTest.js
@@ -0,0 +1,49 @@
+const { WalletCircle, prepareTransaction } = require('../src/index');
+const dotenv = require('dotenv');
+
+async function initcircleWallet(options){
+    const userToken = await WalletCircle.getUserToken(options);
+    options.userToken = userToken.userToken;
+    options.encryptionKey = userToken.encryptionKey;
+    const wallet = new WalletCircle(options);
+    return wallet;
+}
+
+async function main(){
+
+    // configure the env
+    dotenv.config();
+
+    // Initialise the wallet client
+    const wallet = await initcircleWallet({
+        appId: process.env.appId,
+        apiKey: process.env.apiKey,
+        userId: process.env.userId,
+        walletId: process.env.walletId,
+        encryptionKey: process.env.encryptionKey
+    });
+    
+    // Prepare the transaction from expand api
+    // We are making an approve call here
+    const prepareApproveTx = await prepareTransaction('https://api.expand.network/fungibletoken/approve', {
+        "from": "0x6E5eAf34c73D1CD0be4e24f923b97CF38e10d1f3",
+        "tokenAddress": "0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60",
+        "to": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+        "amount": "10000",
+        "gas": "25000",
+        "xApiKey": process.env.xApiKey
+    });
+
+
+    // Securely sign the transaction on user's end
+    const signedTx = await wallet.signTransaction(prepareApproveTx);
+    // Signed Transaction
+    console.log("signedTx: " ,signedTx);
+    // Send the transaction
+    const tx = await wallet.sendTransaction(signedTx);
+    // This challenge id and details can be used to send this transaction through circle on supported networks
+    console.log("sent tx: ", tx);
+}
+
+main();
+
---
+++ test/dfnsTest.js
@@ -0,0 +1,45 @@
+const { WalletDFNS , prepareTransaction } = require('../src/index');
+const dotenv = require('dotenv');
+
+async function main() {
+
+    dotenv.config();
+    let preparedTx =  {
+        "from": "0x4D62A8147c18B45D72BF02378329989533114aDf", 
+        "value": "1000000",                  
+        "to": "0xa67E9B68c41b0f26184D64C26e0b2B81466E5994", 
+        "gas": "100000",
+        "chainId": "5",
+        "xApiKey": xApiKey,
+    };
+    const options = {};
+    options.privateKey = process.env.DFNS_PRIVATE_KEY;
+    options.credId = process.env.DFNS_CRED_ID;
+    options.xApiKey = process.env.xApiKey;
+    options.appId = process.env.DFNS_APP_ID;
+    options.authToken = process.env.DFNS_ACCESS_TOKEN
+    options.baseUrl = process.env.DFNS_API_URL;
+    options.walletId = process.env.WALLET_ID;
+    options.appOrigin = 'http://localhost:3000';
+    // console.log(options);
+    // preparedTx = {
+    //     from: "0x4D62A8147c18B45D72BF02378329989533114aDf",
+    //     to:   "0xa67E9B68c41b0f26184D64C26e0b2B81466E5994",
+    //     data:  "0x",
+    //     value: '100000',
+    //     maxFeePerGas: '120000000000',
+    //     maxPriorityFeePerGas: '120000000000',
+    //     gasLimit: '100000',
+    //     chainId: '5',
+    // }
+    const wallet  = new WalletDFNS(options);
+    // console.log(preparedTx);
+    const signedTx = await wallet.signTransaction(preparedTx);
+    // console.log(signedTx);
+    signedTx.chainId = '5';
+    const tx = await wallet.sendTransaction(signedTx);
+    // console.log(signedTx);
+    console.log("Transaction Pending....", tx);
+}
+    
+main();
