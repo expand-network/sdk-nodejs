c6fb9d66bbe1446d17e0bdabf0ebb5f123c77859
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-03-27 20:18:32 +0200

    Update schema.js

--- configuration/schema.js
+++ configuration/schema.js
@@ -1,7 +1,7 @@
 /*
 * For the functions under chain category, 
 * the validation is set on the function level
-* For example, for funciton getBlock() the request will be same, regardless of the chain ID and symbol 
+* For example, for function getBlock() the request will be same, regardless of the chain ID and symbol 
 * Whereas for all the other category, validations are broken one level down, i.e. to the protocol level
 *
 */
