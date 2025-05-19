e05c283c1c9bcf70904e921a07dabc0714ee61a5
saqib-cmlab <saqib@expand.network>
2025-05-19 13:09:16 +0530

    update aptos for simple transactions

--- src/adapters/Wallet/signTransaction/Aptos.js
+++ src/adapters/Wallet/signTransaction/Aptos.js
@@ -1,5 +1,5 @@
-const { TxnBuilderTypes, BCS, AptosAccount, 
-        AptosClient, HexString } = require('aptos');
+const { Aptos, Ed25519PrivateKey, 
+    AccountAddress, Network, Account } = require("@aptos-labs/ts-sdk");
 const config = require('../../../../configuration/config.json');
 
 module.exports = {
@@ -10,42 +10,50 @@ module.exports = {
          */
 
         try {
-            const { privateKey } = options;
+            let { privateKey } = options;
             const chainId = (options.chainId && options.chainId === "1400") ? "1" : "2";
-
-            const accountFrom = new AptosAccount(HexString.ensure(privateKey).toUint8Array());
+            const aptos = new Aptos({ network:chainId==="1" ? Network.MAINNET : Network.TESTNET });
+            privateKey = new Ed25519PrivateKey(privateKey);
+            const account = Account.fromPrivateKey({ privateKey });
 
             let { data } = transactionObject;
 
-            let transactionBuffer;
-
-            if (!data){
-                const seq = await web3.getAccount(accountFrom.address());
-                transactionBuffer = new TxnBuilderTypes.RawTransaction(
-                    TxnBuilderTypes.AccountAddress.fromHex(transactionObject.from),
-                    BigInt(seq.sequence_number),
-                    new TxnBuilderTypes.TransactionPayloadEntryFunction(
-                        TxnBuilderTypes.EntryFunction.natural(
-                            '0x1::coin',
-                            "transfer",
-                            [new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(config.chains[options.chainId].aptosCoin))],
-                            [BCS.bcsToBytes(TxnBuilderTypes.AccountAddress.fromHex(transactionObject.to)), BCS.bcsSerializeUint64(transactionObject.value)]
-                        ),
-                    ),
-                    options.gas ? options.gas : 1000,
-                    100,
-                    BigInt(Math.floor(Date.now() / 1000) + 10000),
-                    new TxnBuilderTypes.ChainId(chainId),
+            let transaction;
+            let payload;
+            // If `data` is provided, assume it's a base64-encoded JSON payload and decode it
+            if (transactionObject.data) {
+                const decodedPayloadJson = JSON.parse(
+                Buffer.from(transactionObject.data, "base64").toString("utf-8")
                 );
+                payload = {
+                sender: account.accountAddress,
+                data: decodedPayloadJson,
+                }
+                transaction = await aptos.transaction.build.simple({
+                sender: account.accountAddress,
+                data: decodedPayloadJson,
+                });
             } else {
-                const decodedBytes = Buffer.from(data, "base64");
-                const deserializer = new BCS.Deserializer(new Uint8Array(Buffer.from(decodedBytes, "base64")));
-                transactionBuffer = TxnBuilderTypes.RawTransaction.deserialize(deserializer);
+                // Manually build payload
+                const dataPayload = {
+                function: "0x1::coin::transfer",
+                typeArguments: ["0x1::aptos_coin::AptosCoin"],
+                functionArguments: [transactionObject.to, transactionObject.value],
+                };
+                payload = {
+                sender: account.accountAddress,
+                data: dataPayload,
+                }
+                transaction = await aptos.transaction.build.simple({
+                sender: account.accountAddress,
+                data: dataPayload,
+                });
             }
+            const base64Payload = Buffer.from(JSON.stringify(payload)).toString("base64");
+            const signedTxn = await aptos.transaction.sign({ signer: account, transaction });
+            const base64SignedTxn = Buffer.from(signedTxn).toString("base64");
 
-            const bcsTxn = AptosClient.generateBCSTransaction(accountFrom, transactionBuffer);
-
-            const rawTransaction = Buffer.from(bcsTxn).toString("base64");
+            const rawTransaction = [base64Payload, base64SignedTxn]
 
             return { "rawTransaction": rawTransaction };
 
