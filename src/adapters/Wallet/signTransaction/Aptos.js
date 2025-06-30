4a7bb0169455526278a8178d397fd2e150215162
saqib-cmlab <saqib@expand.network>
2025-06-30 17:25:33 +0530

    removed console and format doc

--- src/adapters/Wallet/signTransaction/Aptos.js
+++ src/adapters/Wallet/signTransaction/Aptos.js
@@ -1,59 +1,81 @@
-const { Aptos,  Ed25519PrivateKey, RawTransaction, Deserializer, SimpleTransaction,
-     AptosConfig, Network, Account } = require("@aptos-labs/ts-sdk");
+const {
+  Aptos,
+  Ed25519PrivateKey,
+  RawTransaction,
+  Deserializer,
+  SimpleTransaction,
+  AptosConfig,
+  Network,
+  Account,
+} = require("@aptos-labs/ts-sdk");
 const { batchRequestAptos } = require("../../../helper/batchRequest");
 const TEN_SECONDS = 600;
 module.exports = {
+  signTransactionAptos: async (web3, transactionObject, options) => {
+    /*
+     * Function will sign the transaction payload for Aptos chain
+     */
 
-    signTransactionAptos: async (web3, transactionObject, options) => {
-        /*
-         * Function will sign the transaction payload for Aptos chain
-         */
+    try {
+      let { privateKey } = options;
+      const config = new AptosConfig({
+        network: options.chainId === "1400" ? Network.MAINNET : Network.TESTNET,
+      });
+      const aptos = new Aptos(config);
+      const account = Account.fromPrivateKey({
+        privateKey: new Ed25519PrivateKey(privateKey),
+      });
+      let transaction;
+      if (transactionObject.data) {
+        transaction = new SimpleTransaction(
+          RawTransaction.deserialize(
+            Deserializer.fromHex(transactionObject.data)
+          )
+        );
+      } else {
+        const dataPayload = {
+          function: "0x1::coin::transfer",
+          typeArguments: ["0x1::aptos_coin::AptosCoin"],
+          functionArguments: [
+            transactionObject.to,
+            Number(transactionObject.value),
+          ],
+        };
+        transaction = await aptos.transaction.build.simple({
+          sender: account.accountAddress,
+          data: dataPayload,
+          options: {
+            expireTimestamp: Math.floor(Date.now() / 1000) + TEN_SECONDS,
+          },
+        });
+      }
+      const signedTxn = aptos.transaction.sign({
+        signer: account,
+        transaction,
+      });
+      const signedTxn1 = signedTxn.bcsToHex().toString();
+      const rawTransaction = [transaction.bcsToHex().toString(), signedTxn1];
 
-        try {
-            let { privateKey } = options;
-            const config = new AptosConfig({network:options.chainId==="1400" ? Network.MAINNET : Network.TESTNET});
-            const aptos = new Aptos(config);
-            const account = Account.fromPrivateKey({ privateKey : new Ed25519PrivateKey(privateKey)  });
-            let transaction;
-            if (transactionObject.data) {
-                transaction = new SimpleTransaction(RawTransaction.deserialize(Deserializer.fromHex(transactionObject.data)));
-            } else {
-                const dataPayload = {
-                function: "0x1::coin::transfer",
-                typeArguments: ["0x1::aptos_coin::AptosCoin"],
-                functionArguments: [transactionObject.to, Number(transactionObject.value)],
-                };
-                transaction = await aptos.transaction.build.simple({
-                sender: account.accountAddress,
-                data: dataPayload,
-                options : {
-                            expireTimestamp: Math.floor(Date.now() / 1000) + TEN_SECONDS
-                        }
-                });
-            }
-            const signedTxn = aptos.transaction.sign({ signer: account, transaction });
-            const signedTxn1 = signedTxn.bcsToHex().toString();
-            const rawTransaction = [transaction.bcsToHex().toString(), signedTxn1]
-
-            return { "rawTransaction": rawTransaction };
-
-        }
-        catch (error) {
-            return error;
-        }
-    },
-
-    signSendBatchTransactionsAptos: async (web3, transactionObject, options) => {
-        /*
-         * Function will sign and send the batch the transactions for ethereum based chains
-         */
-
-        try {
-            const transaction = await batchRequestAptos(web3, transactionObject, options.privateKey);
-            return transaction;
-        }
-        catch (error) {
-            return (error);
-        }
+      return { rawTransaction: rawTransaction };
+    } catch (error) {
+      return error;
     }
+  },
+
+  signSendBatchTransactionsAptos: async (web3, transactionObject, options) => {
+    /*
+     * Function will sign and send the batch the transactions for ethereum based chains
+     */
+
+    try {
+      const transaction = await batchRequestAptos(
+        web3,
+        transactionObject,
+        options.privateKey
+      );
+      return transaction;
+    } catch (error) {
+      return error;
+    }
+  },
 };
--- src/helper/batchRequest.js
+++ src/helper/batchRequest.js
@@ -1,5 +1,18 @@
-const {Aptos, AptosConfig, Network, 
-      Account, Ed25519PrivateKey } = require("@aptos-labs/ts-sdk");
+const {
+  Aptos,
+  AptosConfig,
+  Network,
+  Account,
+  Ed25519PrivateKey,
+} = require("@aptos-labs/ts-sdk");
+
+async function getNonce(web3, account) {
+  const [pendingNonce, latestNonce] = await Promise.all([
+    web3.eth.getTransactionCount(account, "pending"),
+    web3.eth.getTransactionCount(account),
+  ]);
+  return Math.max(pendingNonce, latestNonce);
+}
 
 module.exports = {
   batchRequestEvm: async (web3, transactionObject, privateKey) => {
@@ -12,23 +25,38 @@ module.exports = {
       const promises = [];
       let batches = transactions;
 
-      if (typeof transactions === 'object' && transactions !== null && !Array.isArray(transactions)) {
+      if (
+        typeof transactions === "object" &&
+        transactions !== null &&
+        !Array.isArray(transactions)
+      ) {
         batches = Object.values(transactions);
       }
       for (let i = 0; i < batches.length; i++) {
-        const txParams = { ...batches[i], nonce: web3.utils.toHex(initialNonce + i) };
-        const signedTx = await web3.eth.accounts.signTransaction(txParams, privateKey);
+        const txParams = {
+          ...batches[i],
+          nonce: web3.utils.toHex(initialNonce + i),
+        };
+        const signedTx = await web3.eth.accounts.signTransaction(
+          txParams,
+          privateKey
+        );
 
         const promise = new Promise((resolve, reject) => {
-          batch.add(web3.eth.sendSignedTransaction.request(signedTx.rawTransaction, (err, data) => {
-            if (err) {
-              console.error('Error executing transaction:', err);
-              reject(err);
-            } else {
-              console.log('Transaction Sent:', data);
-              resolve(data);
-            }
-          }));
+          batch.add(
+            web3.eth.sendSignedTransaction.request(
+              signedTx.rawTransaction,
+              (err, data) => {
+                if (err) {
+                  console.error("Error executing transaction:", err);
+                  reject(err);
+                } else {
+                  console.log("Transaction Sent:", data);
+                  resolve(data);
+                }
+              }
+            )
+          );
         });
         promises.push(promise);
       }
@@ -37,32 +65,35 @@ module.exports = {
       const transactionHash = await Promise.all(promises);
       return transactionHash;
     } catch (error) {
-      console.error('Batch request failed:', error);
+      console.error("Batch request failed:", error);
       throw error;
     }
   },
   batchRequestAptos: async (web3, transactionObject, privateKey) => {
     try {
-      const config = new AptosConfig({network:options.chainId==="1400" ? Network.MAINNET : Network.TESTNET});
-      const aptos = new Aptos(config); 
-      const account = Account.fromPrivateKey({ privateKey: new Ed25519PrivateKey(privateKey) });
+      const config = new AptosConfig({
+        network: options.chainId === "1400" ? Network.MAINNET : Network.TESTNET,
+      });
+      const aptos = new Aptos(config);
+      const account = Account.fromPrivateKey({
+        privateKey: new Ed25519PrivateKey(privateKey),
+      });
       const { transactions } = transactionObject;
-      const decodedPayloads = Object.values(transactions).map(tx =>
-        JSON.parse(Buffer.from(tx.data, 'base64').toString())
+      const decodedPayloads = Object.values(transactions).map((tx) =>
+        JSON.parse(Buffer.from(tx.data, "base64").toString())
       );
-      try{
+      try {
         await aptos.transaction.batch.forSingleAccount({
-        sender: account,
-        data: decodedPayloads,
+          sender: account,
+          data: decodedPayloads,
         });
-        return "Transaction Successful"
-        }catch(error){
-          return "Transaction Failed"
-        }
-    }
-    catch (error) {
-      console.error('Batch request failed:', error);
+        return "Transaction Successful";
+      } catch (error) {
+        return "Transaction Failed";
+      }
+    } catch (error) {
+      console.error("Batch request failed:", error);
       throw error;
     }
-  }
+  },
 };
