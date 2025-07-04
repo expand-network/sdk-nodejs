4ee149682e79098fc813b400c6616763e4130762
saqib-cmlab <saqib@expand.network>
2025-07-04 11:21:41 +0530

    update aptos

--- src/adapters/Wallet/signTransaction/index.js
+++ src/adapters/Wallet/signTransaction/index.js
@@ -26,6 +26,12 @@ exports.signSendBatchTransactionsEvm = async (web3, transactionObject, options) 
     return rawData;
 };
 
+exports.signSendBatchTransactionsAptos = async (web3, transactionObject, options) => {
+
+    const rawData = await Aptos.signSendBatchTransactionsAptos(web3, transactionObject, options);
+    return rawData;
+};
+
 exports.signTransactionNear = async (web3, transactionObject, options) => {
 
     const rawData = await Near.signTransactionNear(web3, transactionObject, options);
